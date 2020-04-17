7b4d07d969944495cae5a8f925b30bf9880ab290
Ramz Nazarov <Ramzi.13.09.18@gmail.com>
2020-04-17 22:22:30 +0300

    finaly

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -32,10 +32,55 @@ namespace HomeWork._16._04
             array[0] = el;
             return array.Length;
         }
-        public static T[] Slice<T>(T[] array,int beginIndex, int endIndex)
+        public static T[] Slice<T>(T[] array,int beginIndex = 0, int endIndex = 0)
         {
-            T[] arr = new T[0];
-            return arr;
+            T[] arr;
+            if(beginIndex>=0 && endIndex == 0&& beginIndex <= array.Length)
+            {
+                arr = new T[array.Length-beginIndex];
+                for(int i =0; i < arr.Length;i++)
+                {
+                    arr[i] = array[beginIndex];
+                    beginIndex++;
+                }
+                return arr;
+            }
+            else if (beginIndex> array.Length || endIndex > array.Length+1)
+            {
+                return arr = new T[0];
+            }
+            else if(beginIndex < 0)
+            {
+                int a = array.Length - (array.Length - Math.Abs(beginIndex));
+                arr = new T[a];
+                for(int i = 0; i < arr.Length;i++)
+                {
+                    arr[i] = array[array.Length - a];
+                    a--;
+                }
+                return arr;
+            }
+            else if(endIndex < 0 && Math.Abs(endIndex) < array.Length)
+            {
+                arr = new T[array.Length - Math.Abs(endIndex)-beginIndex];
+                for(int i =0; i< arr.Length;i++)
+                {
+                    arr[i] = array[beginIndex];
+                    beginIndex++;
+                }
+                return arr;
+            }
+            else if(beginIndex < endIndex)
+            {
+                arr = new T[array.Length - beginIndex];
+                for(int i = 0; i < arr.Length;i++)
+                {
+                    arr[i] = array[beginIndex];
+                    beginIndex++;
+                }
+                return arr;
+            }
+            else return arr = new T[0];           
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,9 +4,19 @@ namespace HomeWork._16._04
 {
     class Program
     {
+        static void Show<T>(T[] arra)
+        {
+            foreach(T w in arra)
+            {
+                System.Console.Write(w + " ");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] arra = {"5","4","3","2","1"};
+            string[] arr = ArrayHelper.Slice<string>(arra,1,-6);
+            Show(arr);
+            Console.ReadKey();
         }
     }
 }
