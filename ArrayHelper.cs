197207574f832b17c47e1830393f27b735447d8f
Ramz Nazarov <Ramzi.13.09.18@gmail.com>
2020-04-17 19:57:28 +0300

    add static method Pop

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -4,6 +4,11 @@ namespace HomeWork._16._04
 {
     static class ArrayHelper
     {
-        
+        public static T Pop<T>(ref T[] array)
+        {
+            T el = array[array.Length-1];
+            Array.Resize(ref array,array.Length-1);
+            return el;
+        }
     }
 }
