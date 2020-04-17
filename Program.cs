19a3eb684c9722f2ed3625a90a87dcb2dd4ff3f6
Ramz Nazarov <Ramzi.13.09.18@gmail.com>
2020-04-17 22:25:32 +0300

    example added

--- Program.cs
+++ Program.cs
@@ -14,8 +14,13 @@ namespace HomeWork._16._04
         static void Main(string[] args)
         {
             string[] arra = {"5","4","3","2","1"};
-            string[] arr = ArrayHelper.Slice<string>(arra,1,-6);
-            Show(arr);
+            string[] arr = ArrayHelper.Slice<string>(arra);
+            ArrayHelper.Pop<string>(ref arra);
+            Show(arra);
+            System.Console.WriteLine();
+            Show(arra);
+            ArrayHelper.Push<string>(ref arra,arra[0]);
+            Show(arra);
             Console.ReadKey();
         }
     }
