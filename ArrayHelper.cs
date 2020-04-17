932605dff8eec0278e79fa2a98e48220e0277999
Ramz Nazarov <Ramzi.13.09.18@gmail.com>
2020-04-17 19:58:55 +0300

    add static method Shift

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -16,5 +16,13 @@ namespace HomeWork._16._04
             array[array.Length-1] = el;
             return array.Length;
         }
+        public static T Shift<T> (ref T[] array)
+        {
+            T el = array[0];
+            Array.Reverse(array);
+            Array.Resize(ref array, array.Length-1);
+            Array.Reverse(array);
+            return el;
+        }
     }
 }
